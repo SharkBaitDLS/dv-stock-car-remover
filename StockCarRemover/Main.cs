161e763ffb006b9e3ffca68556291cec89adf6e3
Isaac M. Good <imgoodproductions@sbcglobal.net>
2026-06-07 20:42:09 -0700

    Scaffold project based on DV modding template

---
+++ StockCarRemover/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace StockCarRemover;
+
+public static class Main
+{
+	// Unity Mod Manager Wiki: https://wiki.nexusmods.com/index.php/Category:Unity_Mod_Manager
+	private static bool Load(UnityModManager.ModEntry modEntry)
+	{
+		Harmony? harmony = null;
+
+		try
+		{
+			harmony = new Harmony(modEntry.Info.Id);
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+			// Other plugin startup logic
+		}
+		catch (Exception ex)
+		{
+			modEntry.Logger.LogException($"Failed to load {modEntry.Info.DisplayName}:", ex);
+			harmony?.UnpatchAll(modEntry.Info.Id);
+			return false;
+		}
+
+		return true;
+	}
+}
